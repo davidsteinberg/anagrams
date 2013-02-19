608c82e62a031b522c55eeda8949e0da21267259
davidsteinberg <davidandrewsteinberg@gmail.com>
2013-02-19 16:41:06 -0500

    updated files, added more detailed descriptions

--- anagram.php
+++ anagram.php
@@ -1,28 +1,47 @@
 <?php
+/*
+	File:    anagrams.php
+	Author:  David Steinberg
+	Date:    2012.09.28
+	Project: Anagrams
+*/
 
+	// Remove white space from our input
 	$input = preg_replace('/\s*/','',$_POST["input"]);
 
+	// Get which language they are producing anagrams in
 	$language = $_POST["language"];
 
+	// Build the url to call to produce search results
 	$url = "http://www.wordsmith.org/anagram/anagram.cgi?anagram=".$input
 		."&language=".$language
 		."&t=0&d=&include=&exclude=&n=&m=&source=adv&a=y&l=y&q=y&k=0";
 
+	// Hold results in variable and move to the relevant section
 	$page = file_get_contents($url);
 	$page = substr($page,strpos($page,"candidate words found:")+22);
 
 	$words = array();
 
+	// Hop from line break to line break until the end
 	while (strpos($page,"<br>")) {
+
+		// Shorten "page" to next line break
 		$page = substr($page,(strpos($page,"<br>"))+4);
+		
+		// Make sure words in between line breaks begin with a number
+		// Conforms format of search results based on url call
+		// If it does not, we are beyond search results
 		$word = preg_replace('/\s*/','',substr($page,0,(strpos($page,"<br>"))));
 		if (!is_numeric($word[0]))
 			break;
+
+		// Remove the result's number from the word and add it to the array
 		$word = preg_replace('/[0-9.]/','',$word);
 		$words[] = $word;
-		$count++;
 	}
 
+	// Sort word array and send back as JSON
 	sort($words);
 	echo json_encode($words);
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,15 @@
+/*
+	File:    script.js
+	Author:  David Steinberg
+	Date:    2012.09.28
+	Project: Anagrams
+*/
+
 ////////////////////////////////////////////////////////
 //	VARS
 ////////////////////////////////////////////////////////
 
-// Global vars
+// Local global vars
 var text = '';
 var beginning = true;
 
@@ -10,20 +17,26 @@ var beginning = true;
 //	ANAGRAM FUNCTION
 ////////////////////////////////////////////////////////
 
-// To get the anagrams
+// Call to get the anagrams
 function anagram() {
+
 	// Remove current anagrams
 	$("#wordCandidates").empty();
+
 	// Stop if there are no more letters
 	if (text.trim().length == 0) {
 		$("#remainingLetters").html("&nbsp;");
 		$('#wordCandidates').html("Good job! You used all the letters.");
 		return false;
 	}
-	// Show loading gif and remaining letters and make sure to be at top of page
+
+	// Show loading gif and remaining letters
 	$("#loading").css("visibility","visible");
 	$("#remainingLetters").html(text);
+
+	// Make sure to be at top of page
 	$(document).scrollTop(0);
+
 	// Ajax call to anagram.php
 	$.ajax({
 		type: 'POST',
@@ -31,11 +44,15 @@ function anagram() {
 		dataType: 'json',
 		data: { input: text, language: $("#language").val() },
 		success: function(data) {
+
 			// Upon success, populate the anagram list
 			var alphabetList = $('#wordCandidates');
 			alphabetList.empty(); // Clear it first
+
 			var ch = 0;
+
 			for (var key in data) {
+
 				// Put a new drop down header for each new letter
 				if (data[key][0] != ch) {
 					if (ch != 0)
@@ -43,9 +60,11 @@ function anagram() {
 					ch = data[key][0];
 					alphabetList.append('<div data-role="collapsible" data-collapsed-icon="arrow-r" data-theme="b" data-expanded-icon="arrow-d"><h2>'+ch.toUpperCase()+'</h2><ul data-theme="d" name="list" id="list'+ch.toUpperCase()+'"data-role="listview">'); // data-filter="true"
 				}
+
 				// Put in the current word
 				$('#list'+ch.toUpperCase()).append('<li class="word center">'+data[key]+'</li>');
 			}
+
 			// Handle empty anagram list
 			if ($("#wordCandidates").children().length == 0) {
 				if (beginning)
@@ -54,14 +73,17 @@ function anagram() {
 					$('#wordCandidates').html("There are no more words to make with the remaining letters ("+$("#remainingLetters").text().trim()+").");
 			}
 			else {
-				// Close off list and make jQuery stuff pretty/functional again
+				// Close off list
 				alphabetList.append('</ul></div>');
+
+				// Make jQuery elements pretty/functional again
 				$('div[data-role=collapsible]').collapsible();
 				$('ul[data-role=listview]').listview().trigger("create");
 				$("ul").sortable({
 					connectWith: "ul"
 				});
 			}
+
 			// Get rid of loading gif
 			$("#loading").css("visibility","hidden");
 			beginning = false;
@@ -75,6 +97,7 @@ function anagram() {
 
 // Routine to call on submit or language change
 function submitRoutine() {
+
 	// Make sure there is actually text
 	if ($("#text").val().trim() == "") {
 		$("#text").attr("placeholder","Please enter something first.");
@@ -83,11 +106,15 @@ function submitRoutine() {
 	} else {
 		$("#text").attr("placeholder","Type words or names here");
 	}
+
 	beginning = true;
+
 	// Empty the anagram list
 	$("#wordChains").empty();
+
 	// Put the letters into a sorted list
 	text = $('#text').val().split('').sort().toString().replace(/,/g,' ');
+
 	anagram();
 }
 
@@ -97,6 +124,7 @@ function submitRoutine() {
 
 // Upon page load and ready
 $(document).bind('pageinit',function(){
+
 	// Reset areas' heights
 	$("#wordCandidates").css("height",$(window).height()-$("#header").height()-216);
 	$(".recycling").css("height",$(window).height()-$("#header").height()-216);
@@ -104,68 +132,87 @@ $(document).bind('pageinit',function(){
 
 	// Text entry input box
 	$("#text").bind("keypress",function(event){
+
 		// Submit on enter key
 		if (event.which == 13) {
 			// Only keep letters
 			$(this).val($(this).val().replace(/[^a-zA-Z\s]/g,''));
 			$("#submit").trigger("click");
 		}
+
 	}).bind('blur',function(){ 
-		// Only keep letters
+
+		// Only keep letters and spaces when blurred
 		$(this).val($(this).val().replace(/[^a-zA-Z\s]/g,''));
+
 	});
 
 	// Make the saved words drag and droppable
 	$("#wordChains").sortable({
 		receive: function(event, ui) {
+
 			var word = ui.item.text();
+
 			var i = 0;
 			for (i=0; i<word.length; i++)
 				text = text.replace(word[i],'');
 			text = text.split('').sort().toString().replace(/,/g,' ');
+
 			// Show the recycling bin when new items are added
 			$("#recyclingBin").show();
+
 			anagram();
 		}
 	});
 
 	// Handle clicks on anagrams
 	$("#wordCandidates .word").live("click",function(){
+
 		var word = $(this).text();
 		var i = 0;
+
 		// Remove letters of word from remaining letters
 		for (i=0; i<word.length; i++)
 			text = text.replace(word[i],'');
+
 		// Put the letters into a sorted list
 		text = text.split('').sort().toString().replace(/,/g,' ');
+
 		// Save the word
 		$("#wordChains").append('<li class="word center">'+word+'</li>');
 		$("#wordChains").listview("refresh");
 		$("#recyclingBin").show();
+
 		anagram();
 	});
 
 	// Handle click on saved word
 	$("#wordChains .word").live("click",function(){
+
 		// Add letters back in
 		var word = $(this).text();
 		text += word;
 		text = text.split('').sort().toString().replace(/,/g,' ');
+
 		// Get rid of the word
 		$(this).remove();
 		if ($("#wordChains").children().length == 0)
 			$("#recyclingBin").hide();
+
 		anagram();
 	});
 
 	// Handle drag and drop from saved to recycling areas
 	$(".recycling").sortable({
 		receive: function(event, ui) {
+
 			// Add letters back in
 			text += ui.item.text();
 			text = text.split('').sort().toString().replace(/,/g,' ');
+
 			// Get rid of the word
 			ui.item.remove();
+
 			anagram();
 		}
 	});
@@ -173,13 +220,16 @@ $(document).bind('pageinit',function(){
 	// Handle click on recycling bin
 	$("#recyclingBin").live("click",function(){
 		var wordChains = $("#wordChains");
+
 		// Add letters back to the remaining letters and remove the saved words one by one
 		while (wordChains.children().length != 0) {
 			text += wordChains.children(0).text();
 			text = text.split('').sort().toString().replace(/,/g,' ');
 			wordChains.children(0).remove();
 		}
+
 		$("#recyclingBin").hide();
+
 		anagram();
 	});
 	
